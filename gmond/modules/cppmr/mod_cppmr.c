2789a4c6a5f86f816b82623c12ada49ce243adf3
Evgeniy Fominov <evgeniy.fm@gmail.com>
2018-09-19 18:03:44 +0300

    compiled cppmr

--- gmond/modules/cppmr/mod_cppmr.c
+++ gmond/modules/cppmr/mod_cppmr.c
@@ -48,10 +48,7 @@
  */
 extern mmodule cppmr_module;
 
-static unsigned int random_max = 50;
-static unsigned int constant_value = 20;
-
-static key_t key = 7294192;
+static key_t key = 7291192;
 static int shmid = -1;
 #define SHMSZ     1024
 
@@ -62,7 +59,7 @@ struct cppmr_metrics
     uint32_t input_files_read;
 };
 
-static const struct cppmr_metrics *shm = NULL;
+static struct cppmr_metrics *shm = NULL;
 static void open_shm()
 {
     shm = NULL;
@@ -76,8 +73,8 @@ static void open_shm()
         debug_msg("[mod_cppmr]Can't open shared memory handle");
         return;
     }
-    shm = (const struct cppmr_metrics*)shmat(shmid, NULL, 0);
-    if (shm == (const struct cppmr_metrics*)-1)
+    shm = (struct cppmr_metrics*)shmat(shmid, NULL, 0);
+    if (shm == (struct cppmr_metrics*)-1)
     {
         shmid = -1;
         shm = NULL;
@@ -87,9 +84,6 @@ static void open_shm()
 }
 static int ex_metric_init ( apr_pool_t *p )
 {
-    const char* str_params = cppmr_module.module_params;
-    apr_array_header_t *list_params = cppmr_module.module_params_list;
-    mmparam *params;
     int i;
 
     srand(time(NULL)%99);
@@ -138,6 +132,7 @@ static g_val_t ex_metric_handler ( int metric_index )
         break;
     default:
         val.uint32 = 0;
+    }
         
     return val;
 }
@@ -146,10 +141,41 @@ static Ganglia_25metric ex_metric_info[] =
 {
     {0, "input_files_found",      20, GANGLIA_VALUE_UNSIGNED_INT, "Num",   "both", "%u", UDP_HEADER_SIZE+8, "Number of input files discovered"},
     {0, "input_files_read",       20, GANGLIA_VALUE_UNSIGNED_INT, "Num",   "both", "%u", UDP_HEADER_SIZE+8, "Number of input files successfully processed"},
+    {0, "input_bps",              20, GANGLIA_VALUE_FLOAT, "Bytes/sec",    "both", "%.1f", UDP_HEADER_SIZE+8, "Read bytes/sec"},
+    {0, "input_bytes",            20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "Read bytes"},
+    {0, "temp_bps",               20, GANGLIA_VALUE_FLOAT, "Bytes/sec",    "both", "%.1f", UDP_HEADER_SIZE+8, "Temp bytes/sec"},
+    {0, "temp_bytes",             20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "Temp bytes"},
+    {0, "temp_files",             20, GANGLIA_VALUE_UNSIGNED_INT, "Num",   "both", "%u", UDP_HEADER_SIZE+8, "Temp files count"},
+    {0, "temp_chunks",            20, GANGLIA_VALUE_UNSIGNED_INT, "Num",   "both", "%u", UDP_HEADER_SIZE+8, "Temp chunks count"},
+    {0, "output_files",           20, GANGLIA_VALUE_UNSIGNED_INT, "Num",   "both", "%u", UDP_HEADER_SIZE+8, "Output files count"},
+    {0, "reduce_bps",             20, GANGLIA_VALUE_FLOAT, "Bytes/sec",    "both", "%.1f", UDP_HEADER_SIZE+8, "Reduce bytes/sec"},
+    {0, "reduce_bytes",           20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "Reduce bytes"},
+    {0, "output_bps",             20, GANGLIA_VALUE_FLOAT, "Bytes/sec",    "both", "%.1f", UDP_HEADER_SIZE+8, "Output bytes/sec"},
+    {0, "output_bytes",           20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "Output bytes"},
+    {0, "http_in_bps",            20, GANGLIA_VALUE_FLOAT, "Bytes/sec",    "both", "%.1f", UDP_HEADER_SIZE+8, "HTTP IN bytes/sec"},
+    {0, "http_in_bytes",          20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "HTTP IN bytes"},
+    {0, "http_out_bps",           20, GANGLIA_VALUE_FLOAT, "Bytes/sec",    "both", "%.1f", UDP_HEADER_SIZE+8, "HTTP OUT bytes/sec"},
+    {0, "http_out_bytes",         20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "HTTP OUT bytes"},
+    {0, "rows_in",                20, GANGLIA_VALUE_FLOAT, "Rows",         "both", "%.1f", UDP_HEADER_SIZE+8, "Rows read"},
+    {0, "rows_in_speed",          20, GANGLIA_VALUE_FLOAT, "Rows/sec",     "both", "%.1f", UDP_HEADER_SIZE+8, "Rows read per second"},
+    {0, "rows_map",               20, GANGLIA_VALUE_FLOAT, "Rows",         "both", "%.1f", UDP_HEADER_SIZE+8, "Rows map"},
+    {0, "rows_map_speed",         20, GANGLIA_VALUE_FLOAT, "Rows/sec",     "both", "%.1f", UDP_HEADER_SIZE+8, "Rows map per second"},
+    {0, "rows_combine",           20, GANGLIA_VALUE_FLOAT, "Rows",         "both", "%.1f", UDP_HEADER_SIZE+8, "Rows combine"},
+    {0, "rows_combine_speed",     20, GANGLIA_VALUE_FLOAT, "Rows/sec",     "both", "%.1f", UDP_HEADER_SIZE+8, "Rows combine per second"},
+    {0, "rows_temp",              20, GANGLIA_VALUE_FLOAT, "Rows",         "both", "%.1f", UDP_HEADER_SIZE+8, "Rows temp"},
+    {0, "rows_temp_speed",        20, GANGLIA_VALUE_FLOAT, "Rows/sec",     "both", "%.1f", UDP_HEADER_SIZE+8, "Rows temp per second"},
+    {0, "rows_reduce",            20, GANGLIA_VALUE_FLOAT, "Rows",         "both", "%.1f", UDP_HEADER_SIZE+8, "Rows reduce"},
+    {0, "rows_reduce_speed",      20, GANGLIA_VALUE_FLOAT, "Rows/sec",     "both", "%.1f", UDP_HEADER_SIZE+8, "Rows reduce per second"},
+    {0, "rows_output",            20, GANGLIA_VALUE_FLOAT, "Rows",         "both", "%.1f", UDP_HEADER_SIZE+8, "Rows output"},
+    {0, "rows_output_speed",      20, GANGLIA_VALUE_FLOAT, "Rows/sec",     "both", "%.1f", UDP_HEADER_SIZE+8, "Rows output per second"},
+    {0, "heap_size",              20, GANGLIA_VALUE_FLOAT, "Bytes",        "both", "%.1f", UDP_HEADER_SIZE+8, "Heap blocks size, bytes"},
+    {0, "heap_count",             20, GANGLIA_VALUE_UNSIGNED_INT, "Num",   "both", "%u", UDP_HEADER_SIZE+8, "Heap blocks count"},
+    {0, "map_progress",           20, GANGLIA_VALUE_FLOAT, "%",            "both", "%.1f", UDP_HEADER_SIZE+8, "Map stage progress"},
+    {0, "reduce_progress",        20, GANGLIA_VALUE_FLOAT, "%",            "both", "%.1f", UDP_HEADER_SIZE+8, "Reduce stage progress"},
     {0, NULL}
 };
 
-mmodule mux_module =
+mmodule cppmr_module =
 {
     STD_MMODULE_STUFF,
     ex_metric_init,
